79e1fa9f5ea03ab7a323c1a1e66404b9ab6809c5
Scurek <umajdek@gmail.com>
2025-09-17 18:26:38 +0200

    Added page serve

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -1,8 +1,17 @@
 import { FastifyPluginAsync } from "fastify";
+import path from "node:path";
+import fastifyStatic from "@fastify/static";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request, reply) {
-    return { root: true };
+  const distPath = path.resolve(process.cwd(), "..", "client", "dist");
+
+  fastify.register(fastifyStatic, {
+    root: distPath,
+    prefix: "/",
+  });
+
+  fastify.setNotFoundHandler((request, reply) => {
+    reply.sendFile("index.html");
   });
 };
 
