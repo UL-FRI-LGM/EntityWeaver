06b3883f8ac987dff4123e65fb04be3849344660
Scurek <umajdek@gmail.com>
2025-10-29 14:52:36 +0100

    Updated all packages

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,20 +1,22 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 import reactX from "eslint-plugin-react-x";
 import reactDom from "eslint-plugin-react-dom";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
+export default defineConfig([
+  {
+    ignores: ["dist"],
+  },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs.flat["recommended-latest"],
       reactRefresh.configs.vite,
       // Enable lint rules for React
       reactX.configs["recommended-typescript"],
@@ -25,6 +27,7 @@ export default tseslint.config([
       parserOptions: {
         project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         tsconfigRootDir: import.meta.dirname,
+        projectService: true,
       },
       ecmaVersion: 2020,
       globals: globals.browser,
