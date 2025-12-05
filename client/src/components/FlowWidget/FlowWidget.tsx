140dbcfe4906427d0a97a4ef3b6d8faca606edca
Scurek <umajdek@gmail.com>
2025-12-05 11:18:56 +0100

    Updates

--- client/src/components/FlowWidget/FlowWidget.tsx
+++
@@ -1,9 +0,0 @@
-const FlowWidget = () => {
-  return (
-    <div>
-      <h1>Flow</h1>
-    </div>
-  );
-};
-
-export default FlowWidget;
--- server/src/plugins/cors.ts
+++
@@ -1,9 +0,0 @@
-import fp from "fastify-plugin";
-import cors from "@fastify/cors";
-
-export default fp(async (fastify) => {
-  await fastify.register(cors, {
-    origin: ["http://localhost:5173"], // Vite dev server
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  });
-});
