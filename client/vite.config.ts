1c2611970528298c71c1740746fbb05e090c981f
Scurek <umajdek@gmail.com>
2025-12-18 18:03:59 +0100

    Bundle visualizer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr(),
+    tsconfigPaths(),
+    visualizer({
+      filename: "logs/stats.html",
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 });
