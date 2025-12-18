fa423ca7e099cfdcc794a99914f1a2bbc9ac59c1
Scurek <umajdek@gmail.com>
2025-12-18 18:48:03 +0100

    ScrollIntoView

--- client/src/components/DocumentTextView/DocumentTextView.tsx
+++ client/src/components/DocumentTextView/DocumentTextView.tsx
@@ -19,7 +19,7 @@ import { MarkViewContent, type MarkViewRendererProps } from "@tiptap/react";
 import globalClasses from "@/styles/global.module.css";
 import { DEFINES } from "@/defines.ts";
 import { IconPencil } from "@tabler/icons-react";
-import type { Mention } from "@/stores/mention.ts";
+import { Mention } from "@/stores/mention.ts";
 import { useDebouncedCallback } from "@mantine/hooks";
 import { getMarkType } from "@tiptap/core";
 
@@ -45,6 +45,7 @@ const ClickableMarkComponent = observer((props: MarkViewRendererProps) => {
   return (
     <Mark
       ref={markRef}
+      id={mention.id}
       className={clsx(globalClasses.mark, classes.textEntity)}
       style={{
         outline: outline,
@@ -115,161 +116,161 @@ const InsertStarControl = () => {
   );
 };
 
-const TextViewContents = observer(({ document }: { document: Document }) => {
-  const appState = useAppState();
-  const blockNextMentionRerender = useRef<boolean>(false);
-  const blockNextMentionUpdate = useRef<boolean>(false);
+const TextViewContents = observer(
+  ({ documentInstance }: { documentInstance: Document }) => {
+    const appState = useAppState();
+    const blockNextMentionRerender = useRef<boolean>(false);
+    const blockNextMentionUpdate = useRef<boolean>(false);
 
-  const editor = useEditor({
-    extensions: [
-      DocumentExtension,
-      TextExtension,
-      ParagraphExtension,
-      ClickableMark,
-    ],
-    editable: appState.uiState.documentEditMode,
-    onUpdate({ editor }) {
-      // Block here since updateMentions is debounced
-      if (blockNextMentionUpdate.current) {
-        blockNextMentionUpdate.current = false;
+    const editor = useEditor({
+      extensions: [
+        DocumentExtension,
+        TextExtension,
+        ParagraphExtension,
+        ClickableMark,
+      ],
+      editable: appState.uiState.documentEditMode,
+      onUpdate({ editor }) {
+        // Block here since updateMentions is debounced
+        if (blockNextMentionUpdate.current) {
+          blockNextMentionUpdate.current = false;
+          return;
+        }
+        updateMentions(editor);
+      },
+    });
+
+    const updateMentions = useDebouncedCallback((editor: Editor) => {
+      if (appState.viewedDocument === null) {
         return;
       }
-      updateMentions(editor);
-    },
-  });
-
-  const updateMentions = useDebouncedCallback((editor: Editor) => {
-    if (appState.viewedDocument === null) {
-      return;
-    }
-    blockNextMentionRerender.current = true;
-    // console.log("Updating Mentions");
-
-    const markType = getMarkType(ClickableMark.name, editor.schema);
-    appState.viewedDocument.setText(editor.getText());
-    editor.state.doc.descendants((node, pos) => {
-      node.marks.forEach((mark) => {
-        if (mark.type !== markType || node.text === undefined) {
-          return;
-        }
-        const mention = mark.attrs.mention as Mention;
-        const startIndex = pos - 1;
-        const endIndex = startIndex + node.text.length;
-        if (
-          mention.start_index === startIndex &&
-          mention.end_index === endIndex
-        ) {
-          return;
-        }
-        mention.setIndices(startIndex, endIndex);
-      });
-    });
-  }, 500);
-
-  useEffect(() => {
-    if (blockNextMentionRerender.current) {
-      blockNextMentionRerender.current = false;
-      return;
-    }
-    blockNextMentionUpdate.current = true;
-    // console.log("Setting content");
-
-    editor.commands.setContent(`<text>${document.text}</text>`);
-    queueMicrotask(() => {
-      editor.chain().focus();
-
-      const { state, view } = editor;
-      const tr = state.tr;
+      blockNextMentionRerender.current = true;
+      // console.log("Updating Mentions");
 
       const markType = getMarkType(ClickableMark.name, editor.schema);
+      appState.viewedDocument.setText(editor.getText());
+      editor.state.doc.descendants((node, pos) => {
+        node.marks.forEach((mark) => {
+          if (mark.type !== markType || node.text === undefined) {
+            return;
+          }
+          const mention = mark.attrs.mention as Mention;
+          const startIndex = pos - 1;
+          const endIndex = startIndex + node.text.length;
+          if (
+            mention.start_index === startIndex &&
+            mention.end_index === endIndex
+          ) {
+            return;
+          }
+          mention.setIndices(startIndex, endIndex);
+        });
+      });
+    }, 500);
 
-      for (const mention of document.mentionList) {
-        if (mention.end_index - 1 > document.text.length) {
-          console.warn(
-            "Invalid mention indices",
-            mention.end_index,
-            document.text.length,
-          );
-          continue;
-        }
-        try {
-          tr.addMark(
-            mention.start_index + 1,
-            mention.end_index + 1,
-            markType.create({ mention: mention }),
-          );
-        } catch (e) {
-          console.error(e);
-        }
+    useEffect(() => {
+      if (
+        !appState.selectedNodeInstance ||
+        !(appState.selectedNodeInstance instanceof Mention)
+      )
+        return;
+
+      const markElement = document.getElementById(
+        appState.selectedNodeInstance.id,
+      );
+      if (markElement) {
+        markElement.scrollIntoView({ behavior: "smooth", block: "center" });
       }
+    }, [appState, appState.selectedNodeInstance, editor]);
 
-      // document.mentionList.forEach((m) => {
-      //   tr.addMark(
-      //     m.start_index + 1,
-      //     m.end_index + 1,
-      //     markType.create({ mention: m }),
-      //   );
-      // });
-      // console.log("Calling dispatch");
-
+    useEffect(() => {
+      if (blockNextMentionRerender.current) {
+        blockNextMentionRerender.current = false;
+        return;
+      }
       blockNextMentionUpdate.current = true;
-      view.dispatch(tr);
-    });
-  }, [document, editor, document.text, document.mentionList]);
+      // console.log("Setting content");
 
-  return (
-    <>
-      <Title order={2} textWrap="balance">
-        {document.title}
-      </Title>
-      <RichTextEditor
-        variant="subtle"
-        className={classes.editor}
-        classNames={{
-          content: classes.content,
-          root: classes.editorRoot,
-          Typography: classes.editorTypography,
-        }}
-        editor={editor}
-      >
-        <RichTextEditor.Toolbar sticky stickyOffset={0}>
-          <InsertStarControl />
-        </RichTextEditor.Toolbar>
-        <RichTextEditor.Content />
-      </RichTextEditor>
-    </>
-  );
-});
+      editor.commands.setContent(`<text>${documentInstance.text}</text>`);
+      queueMicrotask(() => {
+        editor.chain().focus();
+
+        const { state, view } = editor;
+        const tr = state.tr;
+
+        const markType = getMarkType(ClickableMark.name, editor.schema);
+
+        for (const mention of documentInstance.mentionList) {
+          if (mention.end_index - 1 > documentInstance.text.length) {
+            console.warn(
+              "Invalid mention indices",
+              mention.end_index,
+              documentInstance.text.length,
+            );
+            continue;
+          }
+          try {
+            tr.addMark(
+              mention.start_index + 1,
+              mention.end_index + 1,
+              markType.create({ mention: mention }),
+            );
+          } catch (e) {
+            console.error(e);
+          }
+        }
+
+        // document.mentionList.forEach((m) => {
+        //   tr.addMark(
+        //     m.start_index + 1,
+        //     m.end_index + 1,
+        //     markType.create({ mention: m }),
+        //   );
+        // });
+        // console.log("Calling dispatch");
+
+        blockNextMentionUpdate.current = true;
+        view.dispatch(tr);
+      });
+    }, [
+      documentInstance,
+      editor,
+      documentInstance.text,
+      documentInstance.mentionList,
+    ]);
+
+    return (
+      <>
+        <Title order={2} textWrap="balance">
+          {documentInstance.title}
+        </Title>
+        <RichTextEditor
+          variant="subtle"
+          className={classes.editor}
+          classNames={{
+            content: classes.content,
+            root: classes.editorRoot,
+            Typography: classes.editorTypography,
+          }}
+          editor={editor}
+        >
+          <RichTextEditor.Toolbar sticky stickyOffset={0}>
+            <InsertStarControl />
+          </RichTextEditor.Toolbar>
+          <RichTextEditor.Content />
+        </RichTextEditor>
+      </>
+    );
+  },
+);
 
 const DocumentTextView = observer(() => {
   const appState = useAppState();
-  // const [isTransitionActive, setIsTransitionActive] = useState<boolean>(false);
-  //
-  // function onTransitionStart() {
-  //   setIsTransitionActive(true);
-  // }
-  //
-  // function onTransitionEnd() {
-  //   setIsTransitionActive(false);
-  //   appState.sigma?.refresh();
-  // }
 
   return (
-    <Box
-      className={classes.textView}
-      // style={{
-      //   width: appState.viewedDocument !== null ? 300 : 0,
-      //   padding: appState.viewedDocument !== null ? 20 : 0,
-      // }}
-      // onTransitionStart={onTransitionStart}
-      // onTransitionEnd={onTransitionEnd}
-      // style={{
-      //   width: 300,
-      //   padding: 20,
-      // }}
-    >
+    <Box className={classes.textView}>
       {appState.viewedDocument !== null && (
-        <TextViewContents document={appState.viewedDocument} />
+        <TextViewContents documentInstance={appState.viewedDocument} />
       )}
     </Box>
   );
