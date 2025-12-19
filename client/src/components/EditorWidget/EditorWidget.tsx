79a24e3187e65de46c329514a2d11eb6e7a9b905
Scurek <umajdek@gmail.com>
2025-12-19 12:49:59 +0100

    Package update

--- client/src/components/EditorWidget/EditorWidget.tsx
+++ client/src/components/EditorWidget/EditorWidget.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react";
-import { useAppState } from "@/stores/appState.ts";
+import { type NodeSource, useAppState } from "@/stores/appState.ts";
 import classes from "./EditorWidget.module.css";
 import MentionEditor from "./MentionEditor/MentionEditor.tsx";
 import DocumentEditor from "./DocumentEditor/DocumentEditor.tsx";
@@ -8,28 +8,26 @@ import { Mention } from "@/stores/mention.ts";
 import { Entity } from "@/stores/entity.ts";
 import { Document } from "@/stores/document.ts";
 
+function NodeEditor({ node }: { node: NodeSource }) {
+  if (node instanceof Mention) {
+    return <MentionEditor mention={node} />;
+  }
+  if (node instanceof Document) {
+    return <DocumentEditor document={node} />;
+  }
+  if (node instanceof Entity) {
+    return <EntityEditor entity={node} />;
+  }
+}
+
 const EditorWidget = observer(() => {
   const appState = useAppState();
 
-  function NodeEditor() {
-    const node = appState.selectedNodeInstance;
-    if (!node) return null;
-
-    if (node instanceof Mention) {
-      return <MentionEditor mention={node} />;
-    }
-    if (node instanceof Document) {
-      return <DocumentEditor document={node} />;
-    }
-    if (node instanceof Entity) {
-      return <EntityEditor entity={node} />;
-    }
-  }
-
   return (
     <div className={classes.container}>
-      {/*<h4>Selected Entity</h4>*/}
-      {appState.selectedNodeInstance && <NodeEditor />}
+      {appState.selectedNodeInstance && (
+        <NodeEditor node={appState.selectedNodeInstance} />
+      )}
     </div>
   );
 });
